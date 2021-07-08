a9fe550e69d9bfd6609986f7c829cb2501e9004d
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-08 22:33:25 +0530

    password mandatory

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,14 +1,16 @@
-export interface IUser {
+import { Document } from 'mongoose';
+
+export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
-  profilePicture: string;
-  coverPicture: string;
-  followers: [];
-  followings: [];
-  isAdmin: boolean;
-  desc: string;
-  city: string;
-  from: string;
-  relationship: number;
+  profilePicture?: string;
+  coverPicture?: string;
+  followers?: string[];
+  followings?: string[];
+  isAdmin?: boolean;
+  desc?: string;
+  city?: string;
+  from?: string;
+  relationship?: number;
 }
